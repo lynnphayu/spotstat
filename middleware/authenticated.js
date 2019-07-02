20ce19caf23de22084334533160a9bc56cc98e5f
lynnphayu <mtuthewholewelcome@gmail.com>
2019-07-02 15:00:22 +0630

    token verification fix

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
 	console.log(document.cookie)
-	if (!document.cookie) {
+	if (document.cookie.indexOf('access_token')==-1) {
 	  return redirect('https://spotifystat.herokuapp.com/get_token')
 	}
   }
