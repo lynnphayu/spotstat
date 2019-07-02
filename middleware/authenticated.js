1c0d6b1c06a945b360f061d4319db18b07014669
lynnphayu <mtuthewholewelcome@gmail.com>
2019-07-02 14:57:32 +0630

    url typo fix

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
 	console.log(document.cookie)
 	if (!document.cookie) {
-	  return redirect('https://spotifystat.herokuapp.com//get_token')
+	  return redirect('https://spotifystat.herokuapp.com/get_token')
 	}
   }
