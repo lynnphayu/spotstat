91f4a1aedb52372bf7cd3c5b90be0f049d64794c
lynnphayu <lynnphayu@icloud.com>
2020-10-06 01:14:15 +0630

    redirect refactor

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,9 @@
-export default function ({ store, redirect }) {
+export default function({ store, redirect }) {
   if (document.cookie.indexOf('access_token') == -1) {
-    // return redirect('https://spotifystat.herokuapp.com/get_token')
-    return redirect('http://localhost:3000/get_token')
+    return redirect(
+      process.env.NODE_ENV === 'development'
+        ? 'https://spotifystat.herokuapp.com/get_token'
+        : 'http://localhost:3000/get_token'
+    )
   }
 }
