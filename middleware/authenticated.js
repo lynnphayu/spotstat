7a672c8a93a7ccab9ed5fc5e2663323ec5cde2f8
lynnphayu <lynnphayu@icloud.com>
2020-10-06 01:18:26 +0630

    fix redirect

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,9 +1,9 @@
-export default function({ store, redirect }) {
+export default function ({ store, redirect }) {
   if (document.cookie.indexOf('access_token') == -1) {
     return redirect(
       process.env.NODE_ENV === 'development'
-        ? 'https://spotifystat.herokuapp.com/get_token'
-        : 'http://localhost:3000/get_token'
+        ? 'http://localhost:3000/get_token'
+        : 'https://spotifystat.herokuapp.com/get_token'
     )
   }
 }
